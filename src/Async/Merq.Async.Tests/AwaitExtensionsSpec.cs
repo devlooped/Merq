d0afbb332ffbeababc8f782d60a1258525dc2507
Daniel Cazzulino <daniel@cazzulino.com>
2016-01-11 11:36:30 -0300

    Added missing file

---
+++ src/Async/Merq.Async.Tests/AwaitExtensionsSpec.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Merq.Async.Tests
+{
+	public class AwaitExtensionsSpec
+	{
+		[Fact]
+		public void when_scheduler_is_null_then_throws ()
+		{
+			Assert.Throws<ArgumentNullException> (() => AwaitExtensions.GetAwaiter (null));
+		}
+	}
+}
