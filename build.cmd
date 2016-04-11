ba1264e4ae49e846876dc6f327c38a2b79dd7d93
Daniel Cazzulino <daniel@cazzulino.com>
2016-04-11 09:24:03 -0300

    Auto-detect MSBuild command prompt

--- build.cmd
+++ build.cmd
@@ -1,11 +1,20 @@
-@echo off
-rem Optional batch file to quickly build with some defaults.
-rem Alternatively, this batch file can be invoked passing msbuild parameters, like: build.cmd /v:detailed /t:Rebuild
+:: Optional batch file to quickly build with some defaults.
+:: Alternatively, this batch file can be invoked passing msbuild parameters, like: build.cmd /v:detailed /t:Rebuild
 
-cd %~dp0
+@ECHO OFF
+SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+PUSHD "%~dp0" >NUL
 
-SETLOCAL
 SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
+
+:: Determine if MSBuild can be located. Allows for a better error message below.
+where msbuild > %TEMP%\msbuild.txt
+set /p msb=<%TEMP%\msbuild.txt
+
+IF "%msb%"=="" (
+    echo Please run %~n0 from a Visual Studio Developer Command Prompt.
+    exit /b -1
+)
 
 IF EXIST %CACHED_NUGET% goto copynuget
 echo Downloading latest version of NuGet.exe...
@@ -27,3 +36,7 @@ IF NOT EXIST packages.config goto run
 :run
 rem NOTE: this needs to be a developer command prompt with MSBuild in the path.
 msbuild build.proj /t:build,test,package /v:normal %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+POPD >NUL
+ENDLOCAL
+ECHO ON
