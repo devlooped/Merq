b451e3e2ba1c5e483512e9c5781e37d6ffb9f5a2
Daniel Cazzulino <daniel@cazzulino.com>
2016-05-19 21:05:05 -0300

    Migrate all projects to NuGet v3

--- build.cmd
+++ build.cmd
@@ -28,14 +28,13 @@ copy %CACHED_NUGET% .nuget\nuget.exe > nul
 .nuget\nuget.exe update -self
 
 :restore
-rem Build script packages have no version in the path, so we install them to .nuget\packages to avoid conflicts with 
-rem solution/project packages.
+:: Build script packages have no version in the path, so we install them to .nuget\packages to avoid conflicts with 
+:: solution/project packages.
 IF NOT EXIST packages.config goto run
 .nuget\NuGet.exe install packages.config -OutputDirectory .nuget\packages -ExcludeVersion
 
 :run
-rem NOTE: this needs to be a developer command prompt with MSBuild in the path.
-msbuild build.proj /t:build,test,package /v:normal %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%msb%" build.proj /t:build,test,package /v:normal %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 POPD >NUL
 ENDLOCAL
