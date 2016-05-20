6500856888caecde6653a96a50ac89e4d90ae406
Daniel Cazzulino <daniel@cazzulino.com>
2016-05-20 00:47:44 -0300

    Improve coverage of internal subject wrapper

---
+++ src/Events/Merq.Events.Tests/SubjectWrapperSpec.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Merq
+{
+	public class SubjectWrapperSpec
+	{
+		[Fact]
+		public void when_wrapper_completes_then_inner_subject_completes ()
+		{
+			var subject = new SubjectWrapper<object>();
+			var next = new object();
+			var error = new ArgumentException();
+
+			object onNext = null;
+			bool onCompleted = false;
+
+			subject.Subscribe (o => onNext = o, e => { }, () => onCompleted = true);
+
+			var observer = (IObserver<object>)subject;
+
+			observer.OnNext (next);
+			observer.OnCompleted ();
+
+			Assert.Same (next, onNext);
+			Assert.True (onCompleted);
+		}
+
+		[Fact]
+		public void when_wrapper_errors_then_inner_subject_errors ()
+		{
+			var subject = new SubjectWrapper<object>();
+			var next = new object();
+			var error = new ArgumentException();
+
+			object onNext = null;
+			Exception onError = null;
+
+			subject.Subscribe (o => onNext = o, e => onError = e);
+
+			var observer = (IObserver<object>)subject;
+
+			observer.OnNext (next);
+			observer.OnError (error);
+
+			Assert.Same (next, onNext);
+			Assert.Same (error, onError);
+		}
+	}
+}
--- src/Events/Merq.Events/EventStream.cs
+++ src/Events/Merq.Events/EventStream.cs
@@ -106,40 +106,5 @@ namespace Merq
 		}
 
 		static bool IsValid<TEvent> () => typeof (TEvent).GetTypeInfo().IsPublic || typeof (TEvent).GetTypeInfo().IsNestedPublic;
-
-		abstract class SubjectWrapper
-		{
-			public abstract void OnNext (object value);
-		}
-
-		class SubjectWrapper<T> : SubjectWrapper, ISubject<T>
-		{
-			ISubject<T> subject = new Subject<T>();
-
-			public override void OnNext (object value)
-			{
-				subject.OnNext ((T)value);
-			}
-
-			void IObserver<T>.OnCompleted ()
-			{
-				subject.OnCompleted ();
-			}
-
-			void IObserver<T>.OnError (Exception error)
-			{
-				subject.OnError (error);
-			}
-
-			void IObserver<T>.OnNext (T value)
-			{
-				subject.OnNext (value);
-			}
-
-			IDisposable IObservable<T>.Subscribe (IObserver<T> observer)
-			{
-				return subject.Subscribe (observer);
-			}
-		}
 	}
 }
