106efc2b417c36bf191f817944fda720b0c86369
Daniel Cazzulino <daniel@cazzulino.com>
2016-06-09 11:30:11 -0300

    Turn off echo earlier

--- build.cmd
+++ build.cmd
@@ -1,6 +1,8 @@
 :: Optional batch file to quickly build with some defaults.
 :: Alternatively, this batch file can be invoked passing msbuild parameters, like: build.cmd "/v:detailed" "/t:Rebuild"
 
+@ECHO OFF
+
 :: Ensure MSBuild can be located. Allows for a better error message below.
 where msbuild > %TEMP%\msbuild.txt
 set /p msb=<%TEMP%\msbuild.txt
@@ -10,7 +12,6 @@ IF "%msb%"=="" (
     exit /b -1
 )
 
-@ECHO OFF
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 PUSHD "%~dp0" >NUL
 
