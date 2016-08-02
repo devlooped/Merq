83cd6e2558e941d6769e3565cc83ca592af85907
Daniel Cazzulino <daniel@cazzulino.com>
2016-08-02 00:52:12 -0300

    Add space between Mobile and Essentials in Author field

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -10,11 +10,10 @@
 #pragma warning disable 0436
 [assembly: AssemblyTitle(ThisAssembly.AssemblyName)]
 [assembly: AssemblyDescription(ThisAssembly.AssemblyName)]
-[assembly: AssemblyCompany ("MobileEssentials")]
+[assembly: AssemblyCompany ("Mobile Essentials")]
 [assembly: AssemblyProduct ("Merq")]
-[assembly: AssemblyCopyright ("Copyright © MobileEssentials 2016")]
+[assembly: AssemblyCopyright ("Copyright © Mobile Essentials 2016")]
 
-#pragma warning disable 0436
 [assembly: AssemblyVersion (ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.SemVer.Patch)]
 [assembly: AssemblyFileVersion (ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.SemVer.Patch)]
 [assembly: AssemblyInformationalVersion (ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.SemVer.Patch + "-" + ThisAssembly.Git.Branch + "+" + ThisAssembly.Git.Commit)]
