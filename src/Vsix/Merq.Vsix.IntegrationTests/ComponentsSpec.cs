197bc527af217b7e3b10e3670cd8d48ace2dfdb8
Daniel Cazzulino <daniel@cazzulino.com>
2016-08-06 02:44:42 -0300

    Unify package versions, add MEF test and composition diagnostics
    All packages are now versioned the same.
    When composition fails, leveraging Microsoft.ComponentModel.Composition.Diagnostics.dll
    we can generate a log file that can be used to inspect the failure.

--- src/Vsix/Merq.Vsix.IntegrationTests/ComponentsSpec.cs
+++ src/Vsix/Merq.Vsix.IntegrationTests/ComponentsSpec.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.ComponentModel.Composition.Diagnostics;
 using Microsoft.VisualStudio.ComponentModelHost;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Merq
 {
 	public class ComponentsSpec
 	{
+		ITestOutputHelper output;
+
+		public ComponentsSpec(ITestOutputHelper output)
+		{
+			this.output = output;
+		}
+
+		[InlineData(typeof(IAsyncManager))]
+		[InlineData(typeof(ICommandBus))]
+		[InlineData(typeof(IEventStream))]
+		[VsixTheory]
+		public void when_retrieving_exports_then_reports_duplicate_services(Type serviceType)
+		{
+			var componentModel = GlobalServices.GetService<SComponentModel, IComponentModel>();
+			var contractName = AttributedModelServices.GetContractName(serviceType);
+			var components = componentModel.DefaultExportProvider
+				.GetExports<object, IDictionary<string, object>>(contractName)
+				.ToArray();
+
+			if (components.Length != 1)
+			{
+				var info = new CompositionInfo(componentModel.DefaultCatalog, componentModel.DefaultExportProvider);
+				var log = Path.GetTempFileName();
+				using (var writer = new StreamWriter(log))
+				{
+					CompositionInfoTextFormatter.Write(info, writer);
+					writer.Flush();
+				}
+
+				output.WriteLine(log);
+				// Process.Start(new ProcessStartInfo("notepad", log) { UseShellExecute = true });
+
+				Assert.False(true, $"Expected only one component of {serviceType.Name}. Composition log at {log}");
+			}
+		}
+
 		[VsixFact]
 		public void when_subscribing_and_pushing_events_then_succeeds()
 		{
