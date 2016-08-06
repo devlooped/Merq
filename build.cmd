a87fbdddc8ff0d71aaca7c1660734c0948220c19
Daniel Cazzulino <daniel@cazzulino.com>
2016-08-06 04:45:23 -0300

    Tweak msbuild startup parameters for speed

--- build.cmd
+++ build.cmd
@@ -27,7 +27,7 @@ IF NOT EXIST packages.config goto run
 .nuget\nuget.exe install packages.config -OutputDirectory .nuget\packages -ExcludeVersion -Verbosity quiet
 
 :run
-"%msb%" build.proj /v:minimal %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%msb%" build.proj /v:minimal /nr:true /m %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 POPD >NUL
 ENDLOCAL
