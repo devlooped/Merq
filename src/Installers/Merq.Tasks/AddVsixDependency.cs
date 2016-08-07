7134b21b05fa4f0f2829ea44d9432fab7fd8ec82
Daniel Cazzulino <daniel@cazzulino.com>
2016-08-06 21:01:31 -0300

    Updated installer and AddVsixDependency to support arbitrary VSIXes

--- src/Installers/Merq.Tasks/AddVsixDependency.cs
+++ src/Installers/Merq.Tasks/AddVsixDependency.cs
@@ -21,12 +21,23 @@ namespace Merq.Tasks
 		public override bool Execute()
 		{
 			var xmlns = XNamespace.Get("http://schemas.microsoft.com/developer/vsx-schema/2011");
-			var version = new Lazy<string>(() => XDocument
-				.Load(DependentVsixManifest)
+			var depDoc = XDocument.Load(DependentVsixManifest);
+			if (depDoc.Root.Name.Namespace == xmlns)
+			{
+				Log.LogError("Unsupported document root namespace {0}. Please use a VSIX manifest version 2.0.0 (xmlns='http://schemas.microsoft.com/developer/vsx-schema/2011').", depDoc.Root.Name.NamespaceName);
+				return false;
+			}
+
+			var depId = depDoc
 				.Root
 				.Element(xmlns + "Metadata")
 				.Element(xmlns + "Identity")
-				.Attribute("Version").Value);
+				.Attribute("Id").Value;
+			var depVersion = depDoc
+				.Root
+				.Element(xmlns + "Metadata")
+				.Element(xmlns + "Identity")
+				.Attribute("Version").Value;
 
 			var doc = XDocument.Load(TargetVsixManifest);
 
@@ -39,23 +50,23 @@ namespace Merq.Tasks
 					doc.Root.Add(deps);
 				}
 
-				var dep = deps.Elements(xmlns + "Dependency").FirstOrDefault(e => e.Attribute("Id").Value == "Merq");
+				var dep = deps.Elements(xmlns + "Dependency").FirstOrDefault(e => e.Attribute("Id").Value == depId);
 				if (dep == null)
 				{
-					Log.LogMessage("Adding new dependency on Merq version {0}", version.Value);
+					Log.LogMessage("Adding new dependency on {0} version {1}", depId, depVersion);
 					// <Dependency DisplayName="Merq" Id="Merq" Version="[3.0,)" Location="Merq.vsix" />
 					dep = new XElement(xmlns + "Dependency",
-						new XAttribute("DisplayName", "Merq"),
-						new XAttribute("Id", "Merq"),
-						new XAttribute("Version", "[" + version.Value + ",)"),
-						new XAttribute("Location", "Merq.vsix"));
+						new XAttribute("DisplayName", depId),
+						new XAttribute("Id", depId),
+						new XAttribute("Version", "[" + depVersion + ",)"),
+						new XAttribute("Location", depId + ".vsix"));
 
 					deps.Add(dep);
 				}
 				else
 				{
-					Log.LogMessage("Updating existing dependency on Merq to version {0}", version.Value);
-					dep.Attribute("Version").Value = "[" + version.Value + ",)";
+					Log.LogMessage("Updating existing dependency on {0} to version {1}", depId, depVersion);
+					dep.Attribute("Version").Value = "[" + depVersion + ",)";
 				}
 
 				doc.Save(TargetVsixManifest);
