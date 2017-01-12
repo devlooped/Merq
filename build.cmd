4e5f3442d4e665bec1b99cd9d2bf79eb7b171272
Daniel Cazzulino <daniel@cazzulino.com>
2017-01-11 22:14:29 -0500

    Don't build in parallel, it breaks the VSSDK

--- build.cmd
+++ build.cmd
@@ -35,7 +35,7 @@ IF "%Verbosity%"=="" (
 )
 
 ECHO ON
-%msb% build.proj /v:%Verbosity% /nr:false /maxcpucount /m %1 %2 %3 %4 %5 %6 %7 %8 %9
+%msb% build.proj /v:%Verbosity% /nr:false %1 %2 %3 %4 %5 %6 %7 %8 %9
 @ECHO OFF
 
 POPD >NUL
