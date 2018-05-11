03bff0cd81d553851c1cf92136e69b7cfbdd8888
Daniel Cazzulino <daniel@cazzulino.com>
2018-05-11 18:08:57 -0400

    Migrate all projects to PackageReference, tests to SDK-style

--- src/Core/Merq.Core/CommandBus.cs
+++ src/Core/Merq.Core/CommandBus.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
-using Merq.Properties;
 
 namespace Merq
 {
--- src/Core/Merq.Core/EventStream.cs
+++ src/Core/Merq.Core/EventStream.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Reflection;
-using Merq.Properties;
 
 namespace Merq
 {
