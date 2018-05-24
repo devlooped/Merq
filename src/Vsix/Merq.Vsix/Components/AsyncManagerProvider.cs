fd502f1b14823c795ba84a12d5a5a75b926b3a74
Daniel Cazzulino <daniel@cazzulino.com>
2018-05-24 15:25:54 -0300

    Remove the legacy support and switch to MEF-provided JoinableTaskContext

--- src/Vsix/Merq.Vsix/Components/AsyncManagerProvider.cs
+++ src/Vsix/Merq.Vsix/Components/AsyncManagerProvider.cs
@@ -17,69 +17,16 @@ namespace Merq
 	[PartCreationPolicy (CreationPolicy.Shared)]
 	internal class AsyncManagerProvider
 	{
-		IServiceProvider serviceProvider;
-		Lazy<IAsyncManager> manager;
-
-		/// <summary>
-		/// Initializes the provider with the given VS services.
-		/// </summary>
-		/// <param name="serviceProvider"></param>
 		[ImportingConstructor]
-		public AsyncManagerProvider ([Import (typeof (SVsServiceProvider))] IServiceProvider serviceProvider)
+		public AsyncManagerProvider ([Import] JoinableTaskContext context)
 		{
-			this.serviceProvider = serviceProvider;
-			manager = new Lazy<IAsyncManager> (() => CreateAsyncManager ());
+			AsyncManager = new AsyncManager(context);
 		}
 
 		/// <summary>
 		/// Exports the <see cref="IAsyncManager"/>.
 		/// </summary>
 		[Export]
-		public IAsyncManager AsyncManager => manager.Value;
-
-		IAsyncManager CreateAsyncManager ()
-		{
-			// NOTE: under VS2012, an installer should install the Microsoft.VisualStudio.Threading.Downlevel.vsix 
-			// from the Microsoft.VisualStudio.Threading.DownlevelInstaller nuget package via an MSI.
-			// VS2012 case
-			var context = (JoinableTaskContext)serviceProvider.GetService(typeof(SVsJoinableTaskContext));
-			if (context != null)
-				return new AsyncManager (context);
-
-			// VS2013+ case
-			var schedulerService = (IVsTaskSchedulerService2)serviceProvider.GetService(typeof(SVsTaskSchedulerService));
-			if (schedulerService != null)
-				return new AsyncManager ((JoinableTaskContext)schedulerService.GetAsyncTaskContext ());
-
-			throw new NotSupportedException(Strings.AsyncManagerProvider.NoTaskContext);
-		}
-	}
-}
-
-namespace Microsoft.VisualStudio.Threading
-{
-	/// <summary>
-	/// The legacy VS2012 type serving as the service guid for acquiring an instance of JoinableTaskContext.
-	/// </summary>
-	[Guid("8767A7D4-ECFC-4627-8FC0-A1685E3B0493")]
-	public interface SVsJoinableTaskContext
-	{
-	}
-}
-
-namespace Microsoft.VisualStudio.Shell.Interop
-{
-	/// <summary>
-	/// Internal.
-	/// </summary>
-	[ComImport, Guid("8176DC77-36E2-4987-955B-9F63C6F3F229"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown), TypeIdentifier]
-	public interface IVsTaskSchedulerService2
-	{
-		/// <summary>
-		/// Internal.
-		/// </summary>
-		[return: MarshalAs(UnmanagedType.IUnknown)]
-		[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
-		object GetAsyncTaskContext();
+		public IAsyncManager AsyncManager { get; }
 	}
 }
