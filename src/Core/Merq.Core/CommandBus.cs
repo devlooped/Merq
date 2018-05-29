4c59232cc05a8df89bc5147c1fc5c84094ae9f87
Daniel Cazzulino <daniel@cazzulino.com>
2018-05-29 15:18:08 -0300

    Replace default CommandBus implementation with a much better performing one
    The previous implementation used reflection to invoke the command handlers,
    whereas the new one relies on typed executors for the four invocation patterns
    that are therefore much faster and can be used in mobile without performance
    degradation.

--- src/Core/Merq.Core/CommandBus.cs
+++ src/Core/Merq.Core/CommandBus.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -8,49 +9,133 @@ using System.Threading.Tasks;
 namespace Merq
 {
 	/// <summary>
-	/// Default implementation of the <see cref="ICommandBus"/>.
+	/// Default implementation of the <see cref="ICommandBus"/>, optimized 
+	/// for high performance and no dynamic or reflection invocation.
 	/// </summary>
+	/// <remarks>
+	/// There are basically four types of command/handler pairs: 
+	/// * void synchronous
+	/// * non-void synchronous
+	/// * void async
+	/// * non-void async
+	/// 
+	/// Each of these have different handler interfaces that are constrained 
+	/// by the command type, so that callers can know what invocation style 
+	/// to use depending on the command alone, not the implementation. The 
+	/// implementers are constrained by what's declared in the command type 
+	/// so that there is no mismatch between the invocation style and the 
+	/// implementation style. This avoids implementing anti-patterns like 
+	/// faking async on a non-async implementation and vice-versa. 
+	/// </remarks>
+	/// <devdoc>
+	/// In order for the caller to avoid having to specify both the command 
+	/// type and the result type when executing, we resort to inferring the 
+	/// latter from the former. But in order for this to work, we need to 
+	/// introduce an intermediary "executor" which is typed to the actual 
+	/// command instance received in the various Execute* overloads. 
+	/// 
+	/// This might seem like unnecessary complexity, but it actually produces 
+	/// a very performant invocation pattern (no reflection, lazy instantiation 
+	/// of the executors per command-type) while keeping the calling pattern on 
+	/// the command bus simple like:
+	/// 
+	///     FooResult result = commandBus.Execute(new FooCommand());
+	/// 
+	/// Without the executors, inference wouldn't "just work" and both types 
+	/// (command and result) would need to be specified, as in:
+	/// 
+	///     FooResult result = commandBus.Execute&lt;FooCommand, FooResult&gt;(new FooCommand());
+	/// 
+	/// which is quite awful.
+	/// 
+	/// Each of the four invocation styles gets its own non-generic executor, so that the 
+	/// Execute overloads can invoke them with the generic command argument, as well as the 
+	/// generic implementations that contain the actual invocation and downcast.
+	/// </devdoc>
 	public class CommandBus : ICommandBus
 	{
-		Dictionary<Type, ICommandHandler> handlerMap;
+		ConcurrentDictionary<Type, Lazy<ICommandHandler>> handlerMap = new ConcurrentDictionary<Type, Lazy<ICommandHandler>>();
+		ConcurrentDictionary<Type, VoidExecutor> voidExecutors = new ConcurrentDictionary<Type, VoidExecutor>();
+		ConcurrentDictionary<Type, VoidAsyncExecutor> voidAsyncExecutors = new ConcurrentDictionary<Type, VoidAsyncExecutor>();
+		ConcurrentDictionary<Type, ResultExecutor> resultExecutors = new ConcurrentDictionary<Type, ResultExecutor>();
+		ConcurrentDictionary<Type, ResultAsyncExecutor> resultAsyncExecutors = new ConcurrentDictionary<Type, ResultAsyncExecutor>();
+
+		/// <summary>
+		/// Creates a new <see cref="CommandBus"/> with the given set of command handlers.
+		/// </summary>
+		/// <param name="handlerMap">A map from command types to lazy command handlers.</param>
+		public CommandBus(ConcurrentDictionary<Type, Lazy<ICommandHandler>> handlerMap)
+			=> this.handlerMap = handlerMap;
 
 		/// <summary>
 		/// Initializes the command bus with the given list of handlers.
 		/// </summary>
-		public CommandBus (IEnumerable<ICommandHandler> handlers)
+		public CommandBus(IEnumerable<ICommandHandler> handlers)
+			: this(new ConcurrentDictionary<Type, Lazy<ICommandHandler>>(
+				(handlers ?? throw new ArgumentNullException(nameof(handlers)))
+				.Select(x => new KeyValuePair<Type, Lazy<ICommandHandler>>(GetCommandType(x?.GetType() ?? throw new ArgumentNullException()), new Lazy<ICommandHandler>(() => x)))))
 		{
-			if (handlers == null) throw new ArgumentNullException (nameof (handlers));
-
-
-			AddHandlers (handlers);
 		}
 
 		/// <summary>
 		/// Initializes the command bus with the given list of handlers.
 		/// </summary>
-		public CommandBus (params ICommandHandler[] handlers)
-			: this ((IEnumerable<ICommandHandler>)handlers)
+		public CommandBus(params ICommandHandler[] handlers)
+			: this((IEnumerable<ICommandHandler>)handlers)
 		{
 		}
+
+		/// <summary>
+		/// Registers a command handler type for use with the command bus.
+		/// </summary>
+		public virtual void Register<TCommandHandler>()
+			where TCommandHandler : ICommandHandler, new()
+			=> handlerMap.AddOrUpdate(
+				GetCommandType(typeof(TCommandHandler)),
+				_ => new Lazy<ICommandHandler>(() => new TCommandHandler()),
+				(_, __) => new Lazy<ICommandHandler>(() => new TCommandHandler()));
+
+		/// <summary>
+		/// Registers a command handler instance for use with the command bus.
+		/// </summary>
+		public virtual void Register<TCommandHandler>(TCommandHandler handler)
+			where TCommandHandler : ICommandHandler
+			=> handlerMap.AddOrUpdate(
+				GetCommandType(typeof(TCommandHandler)),
+				_ => new Lazy<ICommandHandler>(() => handler),
+				(_, __) => new Lazy<ICommandHandler>(() => handler));
+
+		/// <summary>
+		/// Registers a lazy command handler instance for use with the command bus.
+		/// </summary>
+		public virtual void Register<TCommandHandler>(Lazy<TCommandHandler> handler)
+			where TCommandHandler : ICommandHandler
+			=> handlerMap.AddOrUpdate(
+				GetCommandType(typeof(TCommandHandler)),
+				_ => new Lazy<ICommandHandler>(() => handler.Value),
+				(_, __) => new Lazy<ICommandHandler>(() => handler.Value));
+
+		static Type GetCommandType(Type type)
+			=> type.GetInterfaces()
+				.Where(t => t.IsGenericType && t.GetGenericTypeDefinition() == typeof(IExecutableCommandHandler<>))
+				.Select(t => t.GetGenericArguments()[0])
+				.FirstOrDefault() ?? throw new ArgumentException();
 
 		/// <summary>
 		/// Determines whether the given command type has a registered handler.
 		/// </summary>
 		/// <typeparam name="TCommand">The type of command to query.</typeparam>
 		/// <returns><see langword="true"/> if the command has a registered handler. <see langword="false"/> otherwise.</returns>
-		public virtual bool CanHandle<TCommand> () where TCommand : IExecutable => handlerMap.ContainsKey (typeof (TCommand));
+		public virtual bool CanHandle<TCommand>() where TCommand : IExecutable
+			=> handlerMap.ContainsKey(typeof(TCommand));
 
 		/// <summary>
 		/// Determines whether the given command has a registered handler.
 		/// </summary>
 		/// <param name="command">The command to query.</param>
 		/// <returns><see langword="true"/> if the command has a registered handler. <see langword="false"/> otherwise.</returns>
-		public virtual bool CanHandle (IExecutable command)
-		{
-			if (command == null) throw new ArgumentNullException (nameof (command));
-
-			return handlerMap.ContainsKey (command.GetType ());
-		}
+		public virtual bool CanHandle(IExecutable command)
+			=> command != null && handlerMap.ContainsKey(command.GetType());
 
 		/// <summary>
 		/// Determines whether the given command can be executed by a registered
@@ -58,29 +143,22 @@ namespace Merq
 		/// </summary>
 		/// <param name="command">The command parameters for the query.</param>
 		/// <returns><see langword="true"/> if the command can be executed. <see langword="false"/> otherwise.</returns>
-		public virtual bool CanExecute<TCommand> (TCommand command) where TCommand : IExecutable
-		{
-			if (command == null) throw new ArgumentNullException (nameof (command));
-
-			var handler = GetCommandHandlerFromMap ((IExecutable) command, throwIfMissing: false);
-
-			return handler == null ? false :
-				((ICanExecute<TCommand>)handler).CanExecute (command);
-		}
+		public virtual bool CanExecute<TCommand>(TCommand command) where TCommand : IExecutable
+			=> command != null &&
+				handlerMap.TryGetValue(command.GetType(), out var handler) &&
+				((ICanExecute<TCommand>)handler.Value).CanExecute(command);
 
 		/// <summary>
 		/// Executes the given command synchronously.
 		/// </summary>
 		/// <param name="command">The command parameters for the execution.</param>
-		public virtual void Execute (ICommand command)
-		{
-			if (command == null) throw new ArgumentNullException (nameof (command));
-
-			ExecuteImpl (
-				"ExecuteCommand",
-				new[] { command.GetType () },
-				new[] { command });
-		}
+		public virtual void Execute(ICommand command)
+			=> voidExecutors.GetOrAdd(
+				GetCommandType(command),
+				type => (VoidExecutor)Activator.CreateInstance(
+					typeof(VoidExecutor<>).MakeGenericType(type),
+					GetCommandHandler(type)))
+				.Execute(command);
 
 		/// <summary>
 		/// Executes the given command synchronously.
@@ -88,30 +166,26 @@ namespace Merq
 		/// <typeparam name="TResult">The return type of the command execution.</typeparam>
 		/// <param name="command">The command parameters for the execution.</param>
 		/// <returns>The result of executing the command.</returns>
-		public virtual TResult Execute<TResult> (ICommand<TResult> command)
-		{
-			if (command == null) throw new ArgumentNullException (nameof (command));
-
-			return (TResult)ExecuteImpl (
-					"ExecuteCommandWithResult",
-					new[] { command.GetType (), typeof (TResult) },
-					new[] { command });
-		}
+		public virtual TResult Execute<TResult>(ICommand<TResult> command)
+			=> (TResult)resultExecutors.GetOrAdd(
+				GetCommandType(command),
+				type => (ResultExecutor)Activator.CreateInstance(
+					typeof(ResultExecutor<,>).MakeGenericType(type, typeof(TResult)),
+					GetCommandHandler(type)))
+				.Execute(command);
 
 		/// <summary>
 		/// Executes the given asynchronous command.
 		/// </summary>
 		/// <param name="command">The command parameters for the execution.</param>
 		/// <param name="cancellation">Cancellation token to cancel command execution.</param>
-		public virtual Task ExecuteAsync (IAsyncCommand command, CancellationToken cancellation)
-		{
-			if (command == null) throw new ArgumentNullException (nameof (command));
-
-			return (Task)ExecuteImpl (
-					"ExecuteCommandAsync",
-					new[] { command.GetType () },
-					new object[] { command, cancellation });
-		}
+		public virtual Task ExecuteAsync(IAsyncCommand command, CancellationToken cancellation)
+			=> voidAsyncExecutors.GetOrAdd(
+				GetCommandType(command),
+				type => (VoidAsyncExecutor)Activator.CreateInstance(
+					typeof(VoidAsyncExecutor<>).MakeGenericType(type),
+					GetCommandHandler(type)))
+				.ExecuteAsync(command, cancellation);
 
 		/// <summary>
 		/// Executes the given command asynchronously.
@@ -120,157 +194,88 @@ namespace Merq
 		/// <param name="command">The command parameters for the execution.</param>
 		/// <param name="cancellation">Cancellation token to cancel command execution.</param>
 		/// <returns>The result of executing the command.</returns>
-		public virtual Task<TResult> ExecuteAsync<TResult> (IAsyncCommand<TResult> command, CancellationToken cancellation)
-		{
-			if (command == null) throw new ArgumentNullException (nameof (command));
+		public virtual Task<TResult> ExecuteAsync<TResult>(IAsyncCommand<TResult> command, CancellationToken cancellation)
+			=> (Task<TResult>)resultAsyncExecutors.GetOrAdd(
+				GetCommandType(command),
+				type => (ResultAsyncExecutor)Activator.CreateInstance(
+					typeof(ResultAsyncExecutor<,>).MakeGenericType(type, typeof(TResult)),
+					GetCommandHandler(type)))
+				.ExecuteAsync(command, cancellation);
 
-			return (Task<TResult>)ExecuteImpl (
-				"ExecuteCommandWithResultAsync",
-				new[] { command.GetType (), typeof (TResult) },
-				new object[] { command, cancellation });
+		/// <summary>
+		/// Command cannot be null for execution
+		/// </summary>
+		static Type GetCommandType(IExecutable command)
+			=> command?.GetType() ?? throw new ArgumentNullException(nameof(command));
+
+		/// <summary>
+		/// A handler must be registered for the given command.
+		/// </summary>
+		ICommandHandler GetCommandHandler(Type commandType)
+			=> handlerMap.TryGetValue(commandType, out var value) ? value.Value : throw new NotSupportedException(commandType.Name);
+
+		abstract class VoidExecutor
+		{
+			public abstract void Execute(IExecutable command);
 		}
 
-		object ExecuteImpl (string methodName, Type[] typeArguments, params object[] parameters)
+		class VoidExecutor<TCommand> : VoidExecutor where TCommand : ICommand
 		{
-			// Important: We need to do this in order to be able to invoke the Execute or ExecuteAsync
-			// methods without specifying the generic arguments by the caller explicitly
-			// Otherwise it would not be possible to do the following:
-			//
-			// commandBus.Execute (new MyCommand ()) // void command
-			// var result = commandBus.execute (new MyCommandWithResult ()) // command with result
+			ICommandHandler<TCommand> handler;
 
-			try {
-				return typeof (CommandBus)
-					.GetTypeInfo ()
-					.GetDeclaredMethod (methodName)
-					.MakeGenericMethod (typeArguments)
-					.Invoke (this, parameters);
-			} catch (TargetInvocationException ex) {
-				// Rethrow the inner exception preserving stack trace.
-				System.Runtime.ExceptionServices.ExceptionDispatchInfo.Capture(ex.InnerException).Throw();
-				// Will never get here.
-				throw ex.InnerException;
-			}
+			public VoidExecutor(ICommandHandler handler)
+				=> this.handler = (ICommandHandler<TCommand>)handler;
+
+			public override void Execute(IExecutable command)
+				=> handler.Execute((TCommand)command);
 		}
 
-		void ExecuteCommand<TCommand> (TCommand command) where TCommand : ICommand
+		abstract class ResultExecutor
 		{
-			var handler = GetCommandHandler(command);
-
-			handler.Execute (command);
+			public abstract object Execute(IExecutable command);
 		}
 
-		TResult ExecuteCommandWithResult<TCommand, TResult> (TCommand command) where TCommand : ICommand<TResult>
+		class ResultExecutor<TCommand, TResult> : ResultExecutor where TCommand : ICommand<TResult>
 		{
-			var handler = GetCommandHandler <TCommand, TResult>(command);
+			ICommandHandler<TCommand, TResult> handler;
 
-			return handler.Execute (command);
+			public ResultExecutor(ICommandHandler handler)
+				=> this.handler = (ICommandHandler<TCommand, TResult>)handler;
+
+			public override object Execute(IExecutable command)
+				=> handler.Execute((TCommand)command);
 		}
 
-		Task ExecuteCommandAsync<TCommand> (TCommand command, CancellationToken cancellation) where TCommand : IAsyncCommand
+		abstract class VoidAsyncExecutor
 		{
-			var handler = GetAsyncCommandHandler (command);
-
-			return handler.ExecuteAsync (command, cancellation);
+			public abstract Task ExecuteAsync(IExecutable command, CancellationToken cancellation);
 		}
 
-		Task<TResult> ExecuteCommandWithResultAsync<TCommand, TResult> (TCommand command, CancellationToken cancellation) where TCommand : IAsyncCommand<TResult>
+		class VoidAsyncExecutor<TCommand> : VoidAsyncExecutor where TCommand : IAsyncCommand
 		{
-			var handler = GetAsyncCommandHandler <TCommand, TResult> (command);
+			IAsyncCommandHandler<TCommand> handler;
 
-			return handler.ExecuteAsync ((dynamic)command, cancellation);
+			public VoidAsyncExecutor(ICommandHandler handler)
+				=> this.handler = (IAsyncCommandHandler<TCommand>)handler;
+
+			public override Task ExecuteAsync(IExecutable command, CancellationToken cancellation)
+				=> handler.ExecuteAsync((TCommand)command, cancellation);
 		}
 
-		void AddHandlers (IEnumerable<ICommandHandler> handlers)
+		abstract class ResultAsyncExecutor
 		{
-			handlerMap = new Dictionary<Type, ICommandHandler> ();
-			var nonGenericHandlers = new List<ICommandHandler>();
-			var duplicateHandlers = new List<Tuple<Type, ICommandHandler>>();
-
-			ProcessHandlers (handlers, nonGenericHandlers, duplicateHandlers);
-			var errors = ProcessErrors (nonGenericHandlers, duplicateHandlers);
-
-			if (!string.IsNullOrEmpty (errors))
-				throw new ArgumentException (errors);
+			public abstract object ExecuteAsync(IExecutable command, CancellationToken cancellation);
 		}
 
-		void ProcessHandlers (IEnumerable<ICommandHandler> handlers, List<ICommandHandler> nonGenericHandlers, List<Tuple<Type, ICommandHandler>> duplicateHandlers)
+		class ResultAsyncExecutor<TCommand, TResult> : ResultAsyncExecutor where TCommand : IAsyncCommand<TResult>
 		{
-			foreach (var handler in handlers.Where (x => x != null)) {
-				// Extracts the first T in any of the command handler interfaces (sync, async, with/without result)
-				var commandType = GetCommandType (handler.GetType ());
-				if (commandType != null) {
-					if (!handlerMap.ContainsKey (commandType))
-						handlerMap.Add (commandType, handler);
-					else
-						// Can't have duplicate handlers either. This is one key 
-						// difference between commands and events.
-						duplicateHandlers.Add (Tuple.Create (commandType, handler));
-				} else {
-					// ICommandHandler without any T is not valid since it can't handle 
-					// anything.
-					nonGenericHandlers.Add (handler);
-				}
-			}
-		}
+			IAsyncCommandHandler<TCommand, TResult> handler;
 
-		static string ProcessErrors (List<ICommandHandler> nonGenericHandlers, List<Tuple<Type, ICommandHandler>> duplicateHandlers)
-		{
-			return string.Join (Environment.NewLine, nonGenericHandlers
-				.Select (handler => Strings.CommandBus.InvalidHandler (handler.GetType ().Name))
-				.Concat (duplicateHandlers
-				.Select (handler => Strings.CommandBus.DuplicateHandler (handler.Item2.GetType ().Name, handler.Item1.Name))));
-		}
+			public ResultAsyncExecutor(ICommandHandler handler)
+				=> this.handler = (IAsyncCommandHandler<TCommand, TResult>)handler;
 
-		ICommandHandler GetCommandHandlerFromMap (IExecutable command, bool throwIfMissing)
-		{
-			ICommandHandler handler;
-			if (!handlerMap.TryGetValue (command.GetType (), out handler) && throwIfMissing) {
-				throw new NotSupportedException (Strings.CommandBus.NoHandler (command.GetType ()));
-			}
-
-			return handler;
-		}
-
-		ICommandHandler<TCommand> GetCommandHandler<TCommand> (TCommand command) where TCommand : ICommand
-		{
-			return (ICommandHandler<TCommand>)GetCommandHandlerFromMap ((IExecutable)command, true);
-		}
-
-		ICommandHandler<TCommand, TResult> GetCommandHandler<TCommand, TResult> (IExecutable command) where TCommand : ICommand<TResult>
-		{
-			return (ICommandHandler<TCommand, TResult>)GetCommandHandlerFromMap (command, true);
-		}
-
-		IAsyncCommandHandler<TCommand> GetAsyncCommandHandler<TCommand> (TCommand command) where TCommand : IAsyncCommand
-		{
-			return (IAsyncCommandHandler<TCommand>)GetCommandHandlerFromMap ((IExecutable)command, true);
-		}
-
-		IAsyncCommandHandler<TCommand, TResult> GetAsyncCommandHandler<TCommand, TResult> (IExecutable command) where TCommand : IAsyncCommand<TResult>
-		{
-			return (IAsyncCommandHandler<TCommand, TResult>)GetCommandHandlerFromMap (command, true);
-		}
-
-		static Type GetCommandType (Type type)
-		{
-			return type.GetTypeInfo ().ImplementedInterfaces
-				.Where (x => IsHandlerInterface (x))
-				.Select (x => x.GenericTypeArguments.First ())
-				.FirstOrDefault ();
-		}
-
-		static bool IsHandlerInterface (Type type)
-		{
-			if (!type.IsConstructedGenericType)
-				return false;
-
-			var genericType = type.GetGenericTypeDefinition();
-
-			return genericType == typeof (ICommandHandler<>) ||
-				genericType == typeof (ICommandHandler<,>) ||
-				genericType == typeof (IAsyncCommandHandler<>) ||
-				genericType == typeof (IAsyncCommandHandler<,>);
+			public override object ExecuteAsync(IExecutable command, CancellationToken cancellation)
+				=> handler.ExecuteAsync((TCommand)command, cancellation);
 		}
 	}
 }
