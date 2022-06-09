d92c7a6de3fc2429c0851425360d414ca3187547
Daniel Cazzulino <daniel@cazzulino.com>
2022-06-09 11:27:59 -0300

    Minor updates and VSSDK bump for dev17 (#5)
    * Fix XML docs
    
    * Ignore pack folder from git
    
    This folder is used for the output of the packaging, so no need to ever
    commit it it
    
    * Simplify InternalsVisibleTo which is built-in the .NET SDK now
    
    * Update VSSDK to latest stable

--- src/Core/Merq/IExecutableCommandHandler.cs
+++ src/Core/Merq/IExecutableCommandHandler.cs
@@ -4,8 +4,9 @@ namespace Merq
 {
 	/// <summary>
 	/// Marker interface for all generic command handlers, whether synchronous or asynchronous, 
-	/// allowing easy introspection of the generic <typeparam name="TCommand" /> if necessary.
+	/// allowing easy introspection of the generic <typeparamref name="TCommand" /> if necessary.
 	/// </summary>
+	/// <typeparam name="TCommand">The type of command to be executed.</typeparam>
 	[EditorBrowsable(EditorBrowsableState.Never)]
 	public interface IExecutableCommandHandler<in TCommand> : ICommandHandler where TCommand : IExecutable
 	{
@@ -13,9 +14,10 @@ namespace Merq
 
 	/// <summary>
 	/// Marker interface for all generic command handlers that return values, whether synchronous or asynchronous, 
-	/// allowing easy introspection of the generic <typeparam name="TCommand" /> and <typeparamref name="TResult"/> 
+	/// allowing easy introspection of the generic <typeparamref name="TCommand" /> and <typeparamref name="TResult"/> 
 	/// if necessary.
 	/// </summary>
+	/// <typeparam name="TCommand">The type of command to be executed.</typeparam>
 	/// <typeparam name="TResult">The type of the command execution result.</typeparam>
 	[EditorBrowsable(EditorBrowsableState.Never)]
 	public interface IExecutableCommandHandler<in TCommand, out TResult> : IExecutableCommandHandler<TCommand> where TCommand : IExecutable<TResult>
