7ca64d8dbb90c6cc2771940da679c85a02df94fd
Daniel Cazzulino <daniel@cazzulino.com>
2022-07-15 19:56:21 -0300

    Fix formatting issue

--- src/Merq.VisualStudio/EventStreamComponent.cs
+++ src/Merq.VisualStudio/EventStreamComponent.cs
@@ -16,17 +16,14 @@ class EventStreamComponent : EventStream
     public EventStreamComponent([Import(typeof(SVsServiceProvider))] IServiceProvider services)
         => components = (IComponentModel)services.GetService(typeof(SComponentModel))!;
 
+    // Since each IObservable component is exported explicitly for each of 
+    // the base classes of the generated event, there will be, for example:
+    // for IObservable<ProjectAdded>, an export for that interface, as well as 
+    // a (say) IObservable<ProjectEvent> and IObservable<BaseEvent>, so that 
+    // when someone subscribes to stream.Of<BaseEvent>, we just need to retrieve 
+    // the concrete IObservable<BaseEvent> here, which considerably simplifies 
+    // the implementation here and avoids caching/invalidation/traversal of 
+    // TEvent hierarchy, etc.
     protected override IEnumerable<IObservable<TEvent>> GetObservables<TEvent>()
-    {
-        // Since each IObservable component is exported explicitly for each of 
-        // the base classes of the generated event, there will be, for example:
-        // for IObservable<ProjectAdded>, an export for that interface, as well as 
-        // a (say) IObservable<ProjectEvent> and IObservable<BaseEvent>, so that 
-        // when someone subscribes to stream.Of<BaseEvent>, we just need to retrieve 
-        // the concrete IObservable<BaseEvent> here, which considerably simplifies 
-        // the implementation here and avoids caching/invalidation/traversal of 
-        // TEvent hierarchy, etc.
-        return components?.GetExtensions<IObservable<TEvent>>() ??
-            Array.Empty<IObservable<TEvent>>();
-    }
+        => components?.GetExtensions<IObservable<TEvent>>() ?? Array.Empty<IObservable<TEvent>>();
 }
