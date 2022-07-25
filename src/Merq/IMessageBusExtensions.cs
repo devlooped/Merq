d1ec4744ee4656276adb86c480e0b5d1db283e2b
Daniel Cazzulino <daniel@cazzulino.com>
2022-07-25 13:58:33 -0300

    Add IMessageBus unifying interface
    The new interface unifies access to both patterns, Commands and Events, so that consumers can take just one dependency.
    
    Fixes #5

---
+++ src/Merq/IMessageBusExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Merq;
+
+/// <summary>
+/// Usability overloads for <see cref="IEventStream"/>.
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class IMessageBusExtensions
+{
+    /// <summary>
+    /// Notifies the bus of a new event instance of <typeparamref name="TEvent"/>.
+    /// </summary>
+    public static void Notify<TEvent>(this IMessageBus bus) where TEvent : new()
+        => bus.Notify(new TEvent());
+}
