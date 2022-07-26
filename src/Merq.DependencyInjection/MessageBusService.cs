19eff42a37ac8e1b41976abf95f451cb0a2392e5
Daniel Cazzulino <daniel@cazzulino.com>
2022-07-26 11:44:57 -0300

    Add initial event implementation to message bus DI
    The new DI project is intended for usage with Microsoft.Extensions.DependencyInjection, and provides the AddMessageBus extension method.
    
    This commit implements the event side of the bus (Observe/Notify) and ports all tests from the EventStream to ensure behavior compatiblity.

---
+++ src/Merq.DependencyInjection/MessageBusService.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Merq;
+
+class MessageBusService : IMessageBus
+{
+    // All subjects active in the event stream.
+    readonly ConcurrentDictionary<Type, Subject> subjects = new();
+    // An cache of subjects indexed by the compatible event types, used to quickly lookup the subjects to 
+    // invoke in a Push. Refreshed whenever a new Of<T> subscription is added.
+    readonly ConcurrentDictionary<Type, Subject[]> compatibleSubjects = new();
+
+    readonly IServiceProvider services;
+
+    public MessageBusService(IServiceProvider services) => this.services = services;
+
+    public bool CanExecute<TCommand>(TCommand command) where TCommand : IExecutable => throw new NotImplementedException();
+    public bool CanHandle<TCommand>() where TCommand : IExecutable => throw new NotImplementedException();
+    public bool CanHandle(IExecutable command) => throw new NotImplementedException();
+    public void Execute(ICommand command) => throw new NotImplementedException();
+    public TResult? Execute<TResult>(ICommand<TResult> command) => throw new NotImplementedException();
+    public Task ExecuteAsync(IAsyncCommand command, CancellationToken cancellation) => throw new NotImplementedException();
+    public Task<TResult> ExecuteAsync<TResult>(IAsyncCommand<TResult> command, CancellationToken cancellation) => throw new NotImplementedException();
+
+    public void Notify<TEvent>(TEvent e)
+    {
+        var type = (e ?? throw new ArgumentNullException(nameof(e))) .GetType();
+        var producer = services.GetService<IObservable<TEvent>>();
+
+        if (producer != null)
+            throw new NotSupportedException($"Cannot explicitly notify event {type} because it is externally produced by {producer.GetType()}.");
+
+        // We call all subjects that are compatible with
+        // the event type, not just concrete event type subscribers.
+        var compatible = compatibleSubjects.GetOrAdd(type, eventType => subjects.Keys
+            .Where(subjectEventType => subjectEventType.IsAssignableFrom(eventType))
+            .Select(subjectEventType => subjects[subjectEventType])
+            .ToArray());
+
+        foreach (var subject in compatible)
+        {
+            subject.OnNext(e);
+        }
+    }
+
+    public IObservable<TEvent> Observe<TEvent>()
+    {
+        var producers = services.GetServices<IObservable<TEvent>>().ToArray();
+        var subject = (IObservable<TEvent>)subjects.GetOrAdd(typeof(TEvent), _ =>
+        {
+            // If we're creating a new subject, we need to clear the cache of compatible subjects
+            compatibleSubjects.Clear();
+            return new Subject<TEvent>();
+        });
+
+        if (producers.Length == 0)
+            return subject;
+
+        return new CompositeObservable<TEvent>(new[] { subject }.Concat(producers).ToArray());
+    }
+
+    abstract class Subject
+    {
+        public abstract void OnNext(object value);
+    }
+
+    class Subject<T> : Subject, IObservable<T>
+    {
+        readonly ConcurrentDictionary<IObserver<T>, object> observers = new();
+
+        public override void OnNext(object value)
+        {
+            var next = (T)value;
+
+            foreach (var item in observers.Keys.ToArray())
+            {
+                // Don't let misbehaving subscribers prevent calling the others
+                try
+                {
+                    item.OnNext(next);
+                }
+                catch (Exception ex)
+                {
+                    // Flag them and remove them to avoid perf. issues from 
+                    // constantly throwing subscribers.
+                    item.OnError(ex);
+                    observers.TryRemove(item, out _);
+                }
+            }
+        }
+
+        IDisposable IObservable<T>.Subscribe(IObserver<T> observer)
+            => new Subscription(this, observer);
+
+        class Subscription : IDisposable
+        {
+            readonly Subject<T> subject;
+            readonly IObserver<T> observer;
+
+            public Subscription(Subject<T> subject, IObserver<T> observer)
+            {
+                this.subject = subject;
+                this.observer = observer;
+                subject.observers.TryAdd(observer, observer);
+            }
+
+            public void Dispose() => subject.observers.TryRemove(observer, out _);
+        }
+    }
+
+    class CompositeObservable<T> : IObservable<T>
+    {
+        readonly IObservable<T>[] observables;
+
+        public CompositeObservable(IObservable<T>[] observables)
+            => this.observables = observables;
+
+        public IDisposable Subscribe(IObserver<T> observer)
+            => new CompositeDisposable(observables
+                .Select(observable => observable.Subscribe(observer)).ToArray());
+    }
+}
---
+++ src/Merq.Tests/MessageBusServiceSpec.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Reactive.Subjects;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Merq;
+
+public record MessageBusServiceSpec(ITestOutputHelper Output)
+{
+    IMessageBus bus = new ServiceCollection()
+        .AddMessageBus()
+        .BuildServiceProvider()
+        .GetRequiredService<IMessageBus>();
+
+    [Fact]
+    public void when_subscribing_external_producer_then_succeeds()
+    {
+        var producer = new Subject<int>();
+        var collection = new ServiceCollection()
+            .AddMessageBus()
+            .AddSingleton<IObservable<int>>(producer);
+
+        var services = collection.BuildServiceProvider();
+        var bus = services.GetRequiredService<IMessageBus>();
+
+        int? value = default;
+
+        bus.Observe<int>().Subscribe(i => value = i);
+
+        producer.OnNext(42);
+
+        Assert.Equal(42, value);
+    }
+
+    [Fact]
+    public void when_notifying_external_producer_then_throws()
+    {
+        var producer = new Subject<int>();
+        var collection = new ServiceCollection()
+            .AddMessageBus()
+            .AddSingleton<IObservable<int>>(producer);
+
+        var services = collection.BuildServiceProvider();
+        var bus = services.GetRequiredService<IMessageBus>();
+
+        Assert.Throws<NotSupportedException>(() => bus.Notify(42));
+    }
+
+    [Fact]
+    public void when_subscribing_subject_then_succeeds()
+    {
+        int? value = default;
+        bus.Observe<int>().Subscribe(i => value = i);
+
+        bus.Notify(42);
+
+        Assert.Equal(42, value);
+    }
+
+    [Fact]
+    public void when_notifying_null_event_then_throws()
+        => Assert.Throws<ArgumentNullException>(() => bus.Notify<object?>(null));
+
+    [Fact]
+    public void when_notifying_non_public_event_type_then_calls_subscriber()
+    {
+        var called = false;
+
+        bus.Observe<NonPublicEvent>().Subscribe(x => called = true);
+
+        bus.Notify(new NonPublicEvent());
+
+        Assert.True(called);
+    }
+
+    [Fact]
+    public void when_notifying_nested_non_public_event_type_then_calls_subscriber()
+    {
+        var called = false;
+
+        bus.Observe<NestedEvent>().Subscribe(x => called = true);
+
+        bus.Notify(new NestedEvent());
+
+        Assert.True(called);
+    }
+
+    [Fact]
+    public void when_notifying_non_subscribed_event_then_does_not_call_subscriber()
+    {
+        var called = false;
+
+        using var subs = bus.Observe<ConcreteEvent>().Subscribe(c => called = true);
+
+        bus.Notify(new AnotherEvent());
+
+        Assert.False(called);
+    }
+
+    [Fact]
+    public void when_notifying_subscribed_event_using_base_type_then_calls_derived_subscriber()
+    {
+        var called = false;
+        using var subscription = bus.Observe<ConcreteEvent>().Subscribe(c => called = true);
+
+        BaseEvent e = new ConcreteEvent();
+        bus.Notify(e);
+
+        Assert.True(called);
+    }
+
+    [Fact]
+    public void when_subscribing_as_event_interface_then_calls_subscriber()
+    {
+        var called = false;
+        using var subs = bus.Observe<IBaseEvent>().Subscribe(c => called = true);
+
+        bus.Notify(new ConcreteEvent());
+
+        Assert.True(called);
+    }
+
+
+    [Fact]
+    public void given_two_observables_when_subscribing_base_event_then_receives_both()
+    {
+        var subject1 = new Subject<ConcreteEvent>();
+        var subject2 = new Subject<AnotherEvent>();
+        var bus = new ServiceCollection()
+            .AddMessageBus()
+            .AddSingleton<IObservable<ConcreteEvent>>(subject1)
+            // NOTE: the producer needs to properly register as the base types too
+            .AddSingleton<IObservable<BaseEvent>>(subject1)
+            .AddSingleton(subject2)
+            .AddSingleton<IObservable<AnotherEvent>>(subject2)
+            .AddSingleton<IObservable<BaseEvent>>(subject2)
+            .BuildServiceProvider()
+            .GetRequiredService<IMessageBus>();
+
+        var called = 0;
+
+        using var subs = bus.Observe<BaseEvent>().Subscribe(c => called++);
+
+        subject1.OnNext(new ConcreteEvent());
+        subject2.OnNext(new AnotherEvent());
+
+        Assert.Equal(2, called);
+    }
+
+    class NestedEvent { }
+}
