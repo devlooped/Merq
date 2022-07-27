f207d07e0d2cb60f5370c6fc7b95dafd7db6a742
Daniel Cazzulino <daniel@cazzulino.com>
2022-07-27 10:43:44 -0300

    Add initial command implementation to message bus DI
    The existing code from CommandBus was ported pretty much entirely,
    
    Removed restriction on Notify for externally produced events, since subscribers can still get it in either case, so this adds consistency.

--- src/Merq.DependencyInjection/MessageBusService.cs
+++ src/Merq.DependencyInjection/MessageBusService.cs
@@ -2,39 +2,129 @@
 using System.Collections.Concurrent;
 using System.Linq;
 using System.Reactive.Disposables;
+using System.Reactive.Subjects;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Merq;
 
+/// <remarks>
+/// There are basically four types of command/handler pairs: 
+/// * void synchronous
+/// * non-void synchronous
+/// * void async
+/// * non-void async
+/// 
+/// Each of these have different handler interfaces that are constrained 
+/// by the command type, so that callers can know what invocation style 
+/// to use depending on the command alone, not the implementation. The 
+/// implementers are constrained by what's declared in the command type 
+/// so that there is no mismatch between the invocation style and the 
+/// implementation style. This avoids implementing anti-patterns like 
+/// faking async on a non-async implementation and vice-versa. 
+/// </remarks>
+/// <devdoc>
+/// In order for the caller to avoid having to specify both the command 
+/// type and the result type when executing, we resort to inferring the 
+/// latter from the former. But in order for this to work, we need to 
+/// introduce an intermediary "executor" which is typed to the actual 
+/// command instance received in the various Execute* overloads. 
+/// 
+/// This might seem like unnecessary complexity, but it actually produces 
+/// a very performant invocation pattern (no reflection, lazy instantiation 
+/// of the executors per command-type) while keeping the calling pattern on 
+/// the command bus simple like:
+/// 
+///     FooResult result = messageBus.Execute(new FooCommand());
+/// 
+/// Without the executors, inference wouldn't "just work" and both types 
+/// (command and result) would need to be specified, as in:
+/// 
+///     FooResult result = messageBus.Execute&lt;FooCommand, FooResult&gt;(new FooCommand());
+/// 
+/// which is quite awful.
+/// 
+/// Each of the four invocation styles gets its own non-generic executor, so that the 
+/// Execute overloads can invoke them with the generic command argument, as well as the 
+/// generic implementations that contain the actual invocation and downcast.
+/// </devdoc>
 class MessageBusService : IMessageBus
 {
+    static readonly MethodInfo canHandleMethod = typeof(MessageBusService).GetMethod(nameof(CanHandle), Type.EmptyTypes);
+
     // All subjects active in the event stream.
     readonly ConcurrentDictionary<Type, Subject> subjects = new();
     // An cache of subjects indexed by the compatible event types, used to quickly lookup the subjects to 
     // invoke in a Push. Refreshed whenever a new Of<T> subscription is added.
     readonly ConcurrentDictionary<Type, Subject[]> compatibleSubjects = new();
 
+    readonly ConcurrentDictionary<Type, bool> canHandleMap = new();
+
+    readonly ConcurrentDictionary<Type, VoidExecutor> voidExecutors = new();
+    readonly ConcurrentDictionary<Type, VoidAsyncExecutor> voidAsyncExecutors = new();
+    readonly ConcurrentDictionary<Type, ResultExecutor> resultExecutors = new();
+    readonly ConcurrentDictionary<Type, ResultAsyncExecutor> resultAsyncExecutors = new();
+
     readonly IServiceProvider services;
 
     public MessageBusService(IServiceProvider services) => this.services = services;
 
-    public bool CanExecute<TCommand>(TCommand command) where TCommand : IExecutable => throw new NotImplementedException();
-    public bool CanHandle<TCommand>() where TCommand : IExecutable => throw new NotImplementedException();
-    public bool CanHandle(IExecutable command) => throw new NotImplementedException();
-    public void Execute(ICommand command) => throw new NotImplementedException();
-    public TResult? Execute<TResult>(ICommand<TResult> command) => throw new NotImplementedException();
-    public Task ExecuteAsync(IAsyncCommand command, CancellationToken cancellation) => throw new NotImplementedException();
-    public Task<TResult> ExecuteAsync<TResult>(IAsyncCommand<TResult> command, CancellationToken cancellation) => throw new NotImplementedException();
+    public bool CanExecute<TCommand>(TCommand command) where TCommand : IExecutable
+        => CanHandle<TCommand>() &&
+           services.GetRequiredService<IExecutableCommandHandler<TCommand>>() is ICanExecute<TCommand> canExec &&
+           canExec.CanExecute(command);
+
+    public bool CanHandle<TCommand>() where TCommand : IExecutable
+        => canHandleMap.GetOrAdd(typeof(TCommand), type
+            => services.GetService<IExecutableCommandHandler<TCommand>>() != null);
+
+    public bool CanHandle(IExecutable command)
+        => canHandleMap.GetOrAdd(command.GetType(), type
+            => (bool)canHandleMethod.MakeGenericMethod(type).Invoke(this, null));
+
+    public void Execute(ICommand command)
+        => voidExecutors.GetOrAdd(
+            GetCommandType(command),
+            type => (VoidExecutor)Activator.CreateInstance(
+                typeof(VoidExecutor<>).MakeGenericType(type),
+                services))
+            .Execute(command);
+
+    public TResult? Execute<TResult>(ICommand<TResult> command)
+        => (TResult)resultExecutors.GetOrAdd(
+            GetCommandType(command),
+            type => (ResultExecutor)Activator.CreateInstance(
+                typeof(ResultExecutor<,>).MakeGenericType(type, typeof(TResult)),
+                services))
+            .Execute(command)!;
+
+    public Task ExecuteAsync(IAsyncCommand command, CancellationToken cancellation)
+        => voidAsyncExecutors.GetOrAdd(
+            GetCommandType(command),
+            type => (VoidAsyncExecutor)Activator.CreateInstance(
+                typeof(VoidAsyncExecutor<>).MakeGenericType(type),
+                services))
+            .ExecuteAsync(command, cancellation);
+
+    public Task<TResult> ExecuteAsync<TResult>(IAsyncCommand<TResult> command, CancellationToken cancellation)
+        => (Task<TResult>)resultAsyncExecutors.GetOrAdd(
+            GetCommandType(command),
+            type => (ResultAsyncExecutor)Activator.CreateInstance(
+                typeof(ResultAsyncExecutor<,>).MakeGenericType(type, typeof(TResult)),
+                services))
+            .ExecuteAsync(command, cancellation);
 
     public void Notify<TEvent>(TEvent e)
     {
-        var type = (e ?? throw new ArgumentNullException(nameof(e))) .GetType();
-        var producer = services.GetService<IObservable<TEvent>>();
+        var type = (e ?? throw new ArgumentNullException(nameof(e))).GetType();
 
-        if (producer != null)
-            throw new NotSupportedException($"Cannot explicitly notify event {type} because it is externally produced by {producer.GetType()}.");
+        // TODO: if we prevent Notify for externally produced events, we won't be 
+        // able to notify base event subscribers when those events are produced. 
+        //var producer = services.GetService<IObservable<TEvent>>();
+        //if (producer != null)
+        //    throw new NotSupportedException($"Cannot explicitly notify event {type} because it is externally produced by {producer.GetType()}.");
 
         // We call all subjects that are compatible with
         // the event type, not just concrete event type subscribers.
@@ -51,8 +141,11 @@ class MessageBusService : IMessageBus
 
     public IObservable<TEvent> Observe<TEvent>()
     {
+        // NOTE: in order for the base event subscription to work properly for external
+        // producers, they must register the service for each T in the TEvent hierarchy.
         var producers = services.GetServices<IObservable<TEvent>>().ToArray();
-        var subject = (IObservable<TEvent>)subjects.GetOrAdd(typeof(TEvent), _ =>
+        
+        var subject = (IObservable<TEvent>)subjects.GetOrAdd(typeof(TEvent).GetTypeInfo(), info =>
         {
             // If we're creating a new subject, we need to clear the cache of compatible subjects
             compatibleSubjects.Clear();
@@ -62,57 +155,14 @@ class MessageBusService : IMessageBus
         if (producers.Length == 0)
             return subject;
 
+        // Merge with any externally-produced observables that are compatible
         return new CompositeObservable<TEvent>(new[] { subject }.Concat(producers).ToArray());
     }
 
-    abstract class Subject
-    {
-        public abstract void OnNext(object value);
-    }
+    static Type GetCommandType(IExecutable command)
+        => command?.GetType() ?? throw new ArgumentNullException(nameof(command));
 
-    class Subject<T> : Subject, IObservable<T>
-    {
-        readonly ConcurrentDictionary<IObserver<T>, object> observers = new();
-
-        public override void OnNext(object value)
-        {
-            var next = (T)value;
-
-            foreach (var item in observers.Keys.ToArray())
-            {
-                // Don't let misbehaving subscribers prevent calling the others
-                try
-                {
-                    item.OnNext(next);
-                }
-                catch (Exception ex)
-                {
-                    // Flag them and remove them to avoid perf. issues from 
-                    // constantly throwing subscribers.
-                    item.OnError(ex);
-                    observers.TryRemove(item, out _);
-                }
-            }
-        }
-
-        IDisposable IObservable<T>.Subscribe(IObserver<T> observer)
-            => new Subscription(this, observer);
-
-        class Subscription : IDisposable
-        {
-            readonly Subject<T> subject;
-            readonly IObserver<T> observer;
-
-            public Subscription(Subject<T> subject, IObserver<T> observer)
-            {
-                this.subject = subject;
-                this.observer = observer;
-                subject.observers.TryAdd(observer, observer);
-            }
-
-            public void Dispose() => subject.observers.TryRemove(observer, out _);
-        }
-    }
+    #region Event Helpers
 
     class CompositeObservable<T> : IObservable<T>
     {
@@ -125,4 +175,74 @@ class MessageBusService : IMessageBus
             => new CompositeDisposable(observables
                 .Select(observable => observable.Subscribe(observer)).ToArray());
     }
+
+    #endregion
+
+    #region Command Executors
+
+    abstract class VoidExecutor
+    {
+        public abstract void Execute(IExecutable command);
+    }
+
+    class VoidExecutor<TCommand> : VoidExecutor where TCommand : ICommand
+    {
+        readonly IServiceProvider services;
+
+        public VoidExecutor(IServiceProvider services)
+            => this.services = services;
+
+        public override void Execute(IExecutable command)
+            => services.GetRequiredService<ICommandHandler<TCommand>>().Execute((TCommand)command);
+    }
+
+    abstract class ResultExecutor
+    {
+        public abstract object? Execute(IExecutable command);
+    }
+
+    class ResultExecutor<TCommand, TResult> : ResultExecutor where TCommand : ICommand<TResult>
+    {
+        readonly IServiceProvider services;
+
+        public ResultExecutor(IServiceProvider services)
+            => this.services = services;
+
+        public override object? Execute(IExecutable command)
+            => services.GetRequiredService<ICommandHandler<TCommand, TResult>>().Execute((TCommand)command);
+    }
+
+    abstract class VoidAsyncExecutor
+    {
+        public abstract Task ExecuteAsync(IExecutable command, CancellationToken cancellation);
+    }
+
+    class VoidAsyncExecutor<TCommand> : VoidAsyncExecutor where TCommand : IAsyncCommand
+    {
+        readonly IServiceProvider services;
+
+        public VoidAsyncExecutor(IServiceProvider services)
+            => this.services = services;
+
+        public override Task ExecuteAsync(IExecutable command, CancellationToken cancellation)
+            => services.GetRequiredService<IAsyncCommandHandler<TCommand>>().ExecuteAsync((TCommand)command, cancellation);
+    }
+
+    abstract class ResultAsyncExecutor
+    {
+        public abstract object ExecuteAsync(IExecutable command, CancellationToken cancellation);
+    }
+
+    class ResultAsyncExecutor<TCommand, TResult> : ResultAsyncExecutor where TCommand : IAsyncCommand<TResult>
+    {
+        readonly IServiceProvider services;
+
+        public ResultAsyncExecutor(IServiceProvider services)
+            => this.services = services;
+
+        public override object ExecuteAsync(IExecutable command, CancellationToken cancellation)
+            => services.GetRequiredService<IAsyncCommandHandler<TCommand, TResult>>().ExecuteAsync((TCommand)command, cancellation);
+    }
+
+    #endregion
 }
