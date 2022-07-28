60488d7ec918fa003a7c28c1e7cd38e2b04ab53b
Daniel Cazzulino <daniel@cazzulino.com>
2022-07-28 16:02:04 -0300

    Add usability helpers for void sync/async commands
    Similar to the Notify<T> where the T is created automatically, provide Execute<T>/ExecuteAsync<T> too.

--- src/Merq/IMessageBusExtensions.cs
+++ src/Merq/IMessageBusExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Merq;
 
@@ -13,4 +15,20 @@ public static class IMessageBusExtensions
     /// </summary>
     public static void Notify<TEvent>(this IMessageBus bus) where TEvent : new()
         => bus.Notify(new TEvent());
+
+    /// <summary>
+    /// Executes the given synchronous command.
+    /// </summary>
+    /// <typeparam name="TCommand">The type of command to create and execute.</typeparam>
+    public static void Execute<TCommand>(this IMessageBus bus) where TCommand : ICommand, new()
+        => bus.Execute(new TCommand());
+
+    /// <summary>
+    /// Executes the given synchronous command.
+    /// </summary>
+    /// <typeparam name="TCommand">The type of command to create and execute.</typeparam>
+    public static Task ExecuteAsync<TCommand>(this IMessageBus bus, CancellationToken cancellation = default)
+        where TCommand : IAsyncCommand, new()
+        => bus.ExecuteAsync(new TCommand(), cancellation);
+
 }
