c18fb0f2f95625a09554f312ee3d99b3ba19f411
Daniel Cazzulino <daniel@cazzulino.com>
2022-07-29 10:05:59 -0300

    Create a unified MessageBus implementation based on IServiceProvider
    This new implementation is more performant than the original CommandBus for public types, since it relies on dynamic dispatch which is faster, as meassured by the new benchmarks project. For the non-public types scenarios, it performs like the original command bus, which is great too and only incurs a downcast.

---
+++ src/Merq.Benchmarks/CoreVsDependency.cs
@@ -0,0 +1,56 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using Merq;
+using Microsoft.Extensions.DependencyInjection;
+
+var summary = BenchmarkRunner.Run<MainBenchmark>();
+
+public class MainBenchmark
+{
+    IMessageBus dibus;
+    IMessageBus corebus;    
+
+    public MainBenchmark()
+    {
+        var handler = new MyCommandHandler();
+        var nonpublic = new MyInternalCommandHandler();
+
+        var services = new ServiceCollection()
+            .AddSingleton<IExecutableCommandHandler<MyCommand>>(handler)
+            .AddSingleton<ICommandHandler<MyCommand>>(handler)
+            .AddSingleton<IExecutableCommandHandler<MyInternalCommand>>(nonpublic)
+            .AddSingleton<ICommandHandler<MyInternalCommand>>(nonpublic);
+
+        dibus = services.AddMessageBus().BuildServiceProvider().GetRequiredService<IMessageBus>();
+        corebus = new MessageBus(services.BuildServiceProvider());
+    }
+
+    [Benchmark]
+    public void CachedDowncastPublicTypes() => dibus.Execute(new MyCommand());
+
+    [Benchmark]
+    public void DynamicDispatchPublicTypes() => corebus.Execute(new MyCommand());
+
+    [Benchmark]
+    public void DowncastDINonPublic() => dibus.Execute(new MyInternalCommand());
+
+    [Benchmark]
+    public void DowncastNonPublic() => corebus.Execute(new MyInternalCommand());
+}
+
+public record MyCommand() : ICommand;
+public class MyCommandHandler : ICommandHandler<MyCommand>
+{
+    public bool CanExecute(MyCommand command) => true;
+
+    public void Execute(MyCommand command) { }
+}
+
+record MyInternalCommand() : ICommand;
+class MyInternalCommandHandler : ICommandHandler<MyInternalCommand>
+{
+    public bool CanExecute(MyInternalCommand command) => true;
+
+    public void Execute(MyInternalCommand command) { }
+}
