0186bd265f152ee43c4fe18a84c228f4555f0728
Daniel Cazzulino <daniel@cazzulino.com>
2022-09-23 02:19:16 -0300

    Add automatic message bus component registrations
    This adds an incremental source generator to the DI package that
    discovers services annotated with [Service(lifetime)] so that they
    can be properly registered in the service collection with the
    right lifetime as well as proper instantiation and registration of the
    required interfaces for the bus to work properly.

---
+++ src/Merq.Tests/GeneratorTests.cs
@@ -0,0 +1,93 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Merq;
+
+public record GeneratorTests(ITestOutputHelper Output)
+{
+    [Fact]
+    public void CanRegisterScopedHandlers()
+    {
+        var collection = new ServiceCollection();
+
+        collection.AddMessageBus();
+
+        var services = collection.BuildServiceProvider();
+
+        Assert.NotNull(services.GetRequiredService<ScopedService>());
+        Assert.NotNull(services.GetRequiredService<ICommandHandler<Command>>());
+        Assert.NotNull(services.GetRequiredService<ICanExecute<Command>>());
+        Assert.NotNull(services.GetRequiredService<IExecutableCommandHandler<Command>>());
+    }
+
+    [Fact]
+    public void CanRegisterTransientHandlers()
+    {
+        var collection = new ServiceCollection();
+
+        collection.AddMessageBus();
+
+        var services = collection.BuildServiceProvider();
+
+        var first = services.GetRequiredService<TransientService>();
+        var second = services.GetRequiredService<TransientService>();
+
+        Assert.NotSame(first, second);
+    }
+
+    [Fact]
+    public void CanRegisterSingletonHandlers()
+    {
+        var collection = new ServiceCollection();
+
+        collection.AddMessageBus();
+
+        var services = collection.BuildServiceProvider();
+
+        var first = services.GetRequiredService<SingletonService>();
+        var second = services.GetRequiredService<SingletonService>();
+        var handler = services.GetRequiredService<ICommandHandler<CommandWithResult, Result>>();
+        var executable = services.GetRequiredService<IExecutableCommandHandler<CommandWithResult, Result>>();
+        var can = services.GetRequiredService<ICanExecute<CommandWithResult>>();
+
+        Assert.Same(first, second);
+        Assert.Same(first, handler);
+        Assert.Same(first, executable);
+        Assert.Same(first, can);
+    }
+}
+
+[Service(ServiceLifetime.Scoped)]
+class ScopedService : ICommandHandler<Command>
+{
+    public bool CanExecute(Command command) => throw new System.NotImplementedException();
+    public void Execute(Command command) => throw new System.NotImplementedException();
+}
+
+
+[Service(ServiceLifetime.Singleton)]
+class SingletonService : ICommandHandler<CommandWithResult, Result>
+{
+    public bool CanExecute(CommandWithResult command) => throw new System.NotImplementedException();
+    public Result Execute(CommandWithResult command) => throw new System.NotImplementedException();
+}
+
+[Service(ServiceLifetime.Transient)]
+class TransientService : IAsyncCommandHandler<AsyncCommand>
+{
+    public bool CanExecute(AsyncCommand command) => throw new System.NotImplementedException();
+    public Task ExecuteAsync(AsyncCommand command, CancellationToken cancellation) => throw new System.NotImplementedException();
+}
+
+[Service(ServiceLifetime.Transient)]
+class TransientService2 : IAsyncCommandHandler<AsyncCommandWithResult, Result>
+{
+    public bool CanExecute(AsyncCommandWithResult command) => throw new System.NotImplementedException();
+    public Task<Result> ExecuteAsync(AsyncCommandWithResult command, CancellationToken cancellation) => throw new System.NotImplementedException();
+}
+
+[Service(ServiceLifetime.Transient)]
+class TransientService3
+{
+}
