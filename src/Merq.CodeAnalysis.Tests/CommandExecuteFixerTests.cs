544ff7e4737d0e2aa176d0df866a5931f6ae2f18
Daniel Cazzulino <daniel@cazzulino.com>
2022-11-09 15:12:02 -0300

    Update analyzer and code fix to instantiate async command
    The Execute<TCommand> shorthand notation cannot be used with async commands, so the fix should also turn the `TCommand` generic parameter into `new TCommand()` argument for `ExecuteAsync`.

--- src/Merq.CodeAnalysis.Tests/CommandExecuteFixerTests.cs
+++ src/Merq.CodeAnalysis.Tests/CommandExecuteFixerTests.cs
@@ -110,6 +110,56 @@ public class CommandExecuteFixerTests
     }
 
     [Fact]
+    public async Task ExecuteGenericSyncWithAsyncCommand()
+    {
+        var test = new CSharpCodeFixTest<CommandExecuteAnalyzer, SyncToAsyncFixer, XUnitVerifier>
+        {
+            TestCode =
+            """
+            using Merq;
+            using System;
+            
+            public record Command : IAsyncCommand;
+            
+            public static class Program
+            {
+                public static void Main()
+                {
+                    var bus = new MessageBus(null);
+                    bus.Execute<{|#0:Command|}>();
+                }
+            }
+            """,
+            FixedCode =
+            """
+            using Merq;
+            using System;
+            
+            public record Command : IAsyncCommand;
+            
+            public static class Program
+            {
+                public static void Main()
+                {
+                    var bus = new MessageBus(null);
+                    {|#0:await bus.ExecuteAsync(new Command())|};
+                }
+            }
+            """
+        }.WithMerq();
+
+        test.ExpectedDiagnostics.Add(new DiagnosticResult(Diagnostics.InvalidSyncOnAsync).WithLocation(0));
+        test.ExpectedDiagnostics.Add(new DiagnosticResult("CS7036", DiagnosticSeverity.Error).WithLocation(11, 13));
+
+        // Don't propagate the expected diagnostics to the fixed code, it will have none of them
+        test.FixedState.InheritanceMode = StateInheritanceMode.Explicit;
+        // NOTE: we don't fix the actual method to make it async too, if needed. C# already provides that.
+        test.FixedState.ExpectedDiagnostics.Add(new DiagnosticResult("CS4033", DiagnosticSeverity.Error).WithLocation(0));
+
+        await test.RunAsync();
+    }
+
+    [Fact]
     public async Task ExecuteAsyncWithSyncCommand()
     {
         var test = new CSharpCodeFixTest<CommandExecuteAnalyzer, AsyncToSyncFixer, XUnitVerifier>
