6e751e069c49a285bed5451a9b47be441705830b
Daniel Cazzulino <daniel@cazzulino.com>
2022-11-17 18:57:01 -0300

    Add test helper that registers a type with all its interfaces

---
+++ src/Merq.Tests/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Merq;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddAll<TImplementation>(this IServiceCollection services)
+        where TImplementation : class, new()
+    {
+        AddAll(services, _ => new TImplementation());
+        return services;
+    }
+
+    public static IServiceCollection AddAll<TImplementation>(
+        this IServiceCollection services, 
+        Func<IServiceProvider, TImplementation> implementationFactory)
+        where TImplementation : class
+    {
+        // Simulates what the DependencyInjection.Attribute does, by registering 
+        // the implementation with all the interfaces it implements.
+        
+        services.AddSingleton(implementationFactory);
+
+        foreach (var interfaceType in typeof(TImplementation).GetInterfaces())
+            services.AddSingleton(interfaceType, s => s.GetRequiredService(typeof(TImplementation)));
+
+        return services;
+    }
+}
